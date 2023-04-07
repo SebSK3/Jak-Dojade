de797bc9a851731b946f96315095791df745736f
sebsk <sebastian@kwasniak.xyz>
2023-04-07 09:57:00 +0000

    missclick

--- src/PathFinder.cpp
+++ src/PathFinder.cpp
@@ -44,7 +44,7 @@ void PathFinder::EdgesBFS(Map *map, City *city, int **road, LinkedList *cities) 
                 }
                 road[row][column] = road[current.y][current.x] + 1;
                 visited.push({column,row});
-#ifdef DEBGUG
+#ifdef DEBUG
                 Helpers::DUMP_ROAD(map, road);
 #endif
                 if (map->lines[row][column] == '*') {
