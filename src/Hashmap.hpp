49bf74176d7378bba15a8ea83e0fdd810cef4396
sebsk <sebastian@kwasniak.xyz>
2023-04-11 07:44:28 +0000

    oho

--- src/Hashmap.hpp
+++ src/Hashmap.hpp
@@ -5,19 +5,25 @@ const int CAPACITY = 10001;
 
 struct Hashmap {
     int size = 0;
-    std::vector<std::pair<char*, City*>>* map; // vector of key-value pairs
-    City **citiesArr;
+    char ***map; // changed to char***
+    City ***citiesArr; // changed to City***
 
     Hashmap() {
-        map = new std::vector<std::pair<char*, City*>>[CAPACITY]; // initialize each bucket as an empty vector
-        citiesArr = new City*[CAPACITY];
+        map = new char**[CAPACITY];
+        citiesArr = new City**[CAPACITY];
+        for (int i = 0; i < CAPACITY; i++) {
+            map[i] = nullptr;
+            citiesArr[i] = nullptr;
+        }
+
     }
 
     ~Hashmap() {
-        delete[] citiesArr;
         for (int i = 0; i < CAPACITY; i++) {
-            map[i].clear(); // free the memory used by each bucket
+            delete[] citiesArr[i];
+            delete[] map[i];
         }
+        delete[] citiesArr;
         delete[] map;
     }
 
@@ -31,21 +37,45 @@ struct Hashmap {
 
     void insert(char *str, City *city) {
         int hashed = hash(str);
-        for (auto& kv : map[hashed]) {
-            if (strcmp(kv.first, str) == 0) { // if the key already exists, update the value
-                kv.second = city;
-                return;
+        if (map[hashed] == nullptr) {
+            map[hashed] = new char*[2];
+            map[hashed][0] = str;
+            map[hashed][1] = nullptr;
+            citiesArr[hashed] = new City*[2];
+            citiesArr[hashed][0] = city;
+            citiesArr[hashed][1] = nullptr;
+        } else {
+            int count = 0;
+            while (map[hashed][count] != nullptr) {
+                if (strcmp(map[hashed][count], str) == 0) {
+                    citiesArr[hashed][count] = city;
+                    return;
+                }
+                count++;
             }
+            char** temp = new char*[count + 2];
+            City** tempCity = new City*[count + 2];
+            memcpy(temp, map[hashed], count * sizeof(char*));
+            memcpy(tempCity, citiesArr[hashed], count * sizeof(City*));
+            temp[count] = str;
+            temp[count + 1] = nullptr;
+            tempCity[count] = city;
+            tempCity[count + 1] = nullptr;
+            delete[] map[hashed];
+            delete[] citiesArr[hashed];
+            map[hashed] = temp;
+            citiesArr[hashed] = tempCity;
         }
-        map[hashed].emplace_back(str, city); // insert the new key-value pair
-        citiesArr[hashed] = city;
     }
 
     City *find(char *str) {
         int hashed = hash(str);
-        for (auto& kv : map[hashed]) {
-            if (strcmp(kv.first, str) == 0) {
-                return kv.second;
+        if (map[hashed] == nullptr) {
+            return NULL;
+        }
+        for (int i = 0; map[hashed][i] != nullptr; i++) {
+            if (strcmp(map[hashed][i], str) == 0) {
+                return citiesArr[hashed][i];
             }
         }
         return NULL;
