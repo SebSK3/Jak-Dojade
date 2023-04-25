52d3a9dae5de61e07c2b26f1f4fe37e58625ffd8
sebsk <sebastian@kwasniak.xyz>
2023-04-25 05:48:01 +0000

    fix new input

--- src/Helpers.hpp
+++ src/Helpers.hpp
@@ -6,13 +6,13 @@
 namespace Helpers {
 bool InsideMap(const Map &map, Position coords);
 bool IsCharacter(char c);
+bool isNotWeirdAsciiChar(char c);
 City *FindCityByPos(LinkedList<City *> *cities, Position pos);
 char *BuildCityName(const Map &map, Position coords);
 unsigned int CITY_ID(bool firstUsed = false, bool shouldIncrement = true);
 #ifdef DEBUG
 void DUMP_CITY(City *city);
 void DUMP_GRAPH(City *city);
-void DUMP_LIST(LinkedList *list);
 void DUMP_ROAD(Map *map, int **road);
 #endif
 } // namespace Helpers
